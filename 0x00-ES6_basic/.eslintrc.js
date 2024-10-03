6a5195b317062184378e362a210872db50b54ea3
OduogoWalter <oduogowalter@gmail.com>
2024-10-03 11:11:28 +0300

    ES6_basic

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2015,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  };
+  
