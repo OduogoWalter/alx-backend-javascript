a1d4265afb215bb8433c28470a274a4df1be6a59
OduogoWalter <oduogowalter@gmail.com>
2024-10-03 11:21:27 +0300

    ES6_basic

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
     env: {
-      browser: true,
+      browser: false,
       es6: true,
-      node: true,
+      jest: true,
     },
-    extends: ['eslint:recommended'],
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
-      ecmaVersion: 2015,
+      ecmaVersion: 2018,
       sourceType: 'module',
     },
+    plugins: ['jest'],
     rules: {
-      'no-var': 'error',
-      'prefer-const': 'error',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
     },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
   };
   
