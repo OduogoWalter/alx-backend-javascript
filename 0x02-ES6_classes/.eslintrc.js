f22892b3712322a23c5e9d2a01d79a61b38ed78c
OduogoWalter <oduogowalter@gmail.com>
2024-11-22 13:43:34 +0300

    0-main.js

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -1,23 +1,37 @@
 module.exports = {
   env: {
+    browser: false,
     es6: true,
-    node: true,
-    jest: true
+    jest: true,
   },
   extends: [
-    'airbnb-base'
+    'airbnb-base',
+    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    'linebreak-style': ['error', 'windows'],
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
     'no-console': 'off',
-    'quotes': ['error', 'double', { 'allowTemplateLiterals': true }]
-  }
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
